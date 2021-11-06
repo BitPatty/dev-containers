c01e9bc1e444d0d0e73250acaa69b0c132b735d0
Matteias Collet <matteias.collet@bluewin.ch>
2021-11-06 17:05:26 +0100

    fix mariadb init scripts

--- mariadb/latest/init.sql
+++ mariadb/latest/init.sql
@@ -1,5 +1,5 @@
-CREATE USER 'devel'@'%' IDENTIFIED WITH mysql_native_password BY 'devel';
-CREATE USER 'devel'@'localhost' IDENTIFIED WITH mysql_native_password BY 'devel';
+CREATE USER 'devel'@'%' IDENTIFIED BY 'devel';
+CREATE USER 'devel'@'localhost' IDENTIFIED BY 'devel';
 
 GRANT ALL PRIVILEGES ON *.* TO 'devel'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'devel'@'localhost';
